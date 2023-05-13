0088a234e1a605cad8967536b86cdbd0306bbec9
Nikhil-Koganti <nikhil.sunny731@gmail.com>
2023-05-13 14:37:59 -0700

    Added and fixed npm packages

--- src/Theme.js
+++ src/Theme.js
@@ -1,39 +1,41 @@
-import {createTheme} from '@material-ui/core/styles';
+import {createTheme} from '@mui/material/styles';
 
  export const theme = createTheme({
     palette: {
-        primary:{   //blue
-            100: "#014E5F",
-            200: "#028090",
-            300: '#003F4C',
-            400:  '#006D82', 
-            500: '#0098B5',
-            600:  '#00C3E0',
+        palette: {
+            primary: {
+              main: "#014E5F",
+              200: "#028090",
+              300: "#003F4C",
+              400: "#006D82",
+              500: "#0098B5",
+              600: "#00C3E0",
+            },
+            secondary: {
+              main: "#4CAF50",
+              50: "#E8F5E9",
+              100: "#C8E6C9",
+              200: "#A5D6A7",
+              300: "#81C784",
+              400: "#66BB6A",
+              600: "#43A047",
+              700: "#388E3C",
+              800: "#2E7D32",
+              900: "#1B5E20",
+            },
+            gray: {
+              main: "#9E9E9E",
+              50: "#FAFAFA",
+              100: "#F5F5F5",
+              200: "#EEEEEE",
+              300: "#E0E0E0",
+              400: "#BDBDBD",
+              600: "#757575",
+              700: "#616161",
+              800: "#424242",
+              900: "#212121",
+            },
         },
-        secondary:{ //green
-                50: '#E8F5E9',
-                100: '#C8E6C9',
-                200: '#A5D6A7',
-                300: '#81C784',
-                400: '#66BB6A',
-                500: '#4CAF50',
-                600: '#43A047',
-                700: '#388E3C',
-                800: '#2E7D32',
-                900: '#1B5E20',
-              }, 
-        gray: { //gray for background mostly
-            50: "#FAFAFA",
-            100: "#F5F5F5",
-            200: "#EEEEEE",
-            300: "#E0E0E0",
-            400: "#BDBDBD",
-            500: "#9E9E9E",
-            600: "#757575",
-            700: "#616161",
-            800: "#424242",
-            900: "#212121"
-          }
     },
     typography: {
         fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
